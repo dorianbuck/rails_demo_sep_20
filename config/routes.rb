2f7b9a74baeaf9bed6cd59fa5fe39f104911299f
Dorian Buck <dorianbuck@gmail.com>
2021-09-21 14:33:52 +0200

    adds implementation code for making test pass for retreiving articles from db

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   namespace :api do
     resources :articles, only: :index
   end
- # get '/api/articles', controller: :articles, action: :index
+  # get '/api/articles', controller: :articles, action: :index
 end
--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    author { "MyString" }
-    lede { "MyText" }
-    image { "MyString" }
+    title { 'MyString' }
+    author { 'MyString' }
+    lede { 'MyText' }
+    image { 'MyString' }
   end
 end
